9872524a2bedf0d320c9fc2cba5d69fe73e6fd94
ksx919 <1983262652@qq.com>
2025-09-22 16:51:14 +0800

    创建springboot启动类，添加数据库以及langchian4j配置

---
+++ Huanyu/src/main/java/com/tanxian/HuanyuApplication.java
@@ -0,0 +1,11 @@
+package com.tanxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuanyuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HuanyuApplication.class, args);
+    }
+}
