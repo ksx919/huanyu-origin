b697c832c1e8d947482b1c9630723eb0e41fe492
ksx919 <1983262652@qq.com>
2025-09-23 23:50:04 +0800

    优化消息存储的实现，在数据库中仅存储用户消息和AI回答，而不需要重复存储系统消息

--- Huanyu/src/main/java/com/tanxian/config/LangChain4jConfig.java
+++ Huanyu/src/main/java/com/tanxian/config/LangChain4jConfig.java
@@ -1,16 +1,19 @@
 package com.tanxian.config;
 
-import com.tanxian.service.MyChatMemoryStore;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.ChatMemoryStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LangChain4jConfig {
 
+    @Autowired
+    private ChatMemoryStore myChatMemoryStore;
     @Bean
-    public ChatMemoryProvider chatMemoryProvider(MyChatMemoryStore myChatMemoryStore) {
+    public ChatMemoryProvider chatMemoryProvider(){
         return memoryId -> MessageWindowChatMemory.builder()
                 .id(memoryId)
                 .maxMessages(20)
--- Huanyu/src/main/java/com/tanxian/exception/BusinessExceptionEnum.java
+++ Huanyu/src/main/java/com/tanxian/exception/BusinessExceptionEnum.java
@@ -1,11 +1,8 @@
 package com.tanxian.exception;
 
 public enum BusinessExceptionEnum {
-    CHAT_TYPE_ERROR("聊天类型传入错误"),
-    AI_CHAT_TYPE_ERROR("AI聊天类型错误"),
-    SYSTEM_MESSAGE_TEMPLATE_NOT_FOUND("系统消息模板未找到"),
-    SYSTEM_MESSAGE_TEMPLATE_LOAD_FAILED("系统消息模板加载失败"),
-    UNSUPPORTED_CHARACTER_TYPE("不支持的角色类型");
+    //聊天类型传入错误
+    CHAT_TYPE_ERROR("聊天类型传入错误"),;
 
     BusinessExceptionEnum(String desc) {
         this.desc = desc;
